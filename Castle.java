98b4dc23144220c6eeec8c609b174172ff0c1d6d
elenaa999 <350214680@gapps.yrdsb.ca>
2023-12-13 12:33:53 -0500

    Choosing background for the game
    //Choosing castle as the image of background

---
+++ Castle.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Castle here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Castle extends Actor
+{
+    /**
+     * Act - do whatever the Castle wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        //Castle as the background for this project.
+    }
+}
