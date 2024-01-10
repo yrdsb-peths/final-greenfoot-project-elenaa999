280a7c00d3cf418582e054c57dd930d8675945ee
elenaa999 <350214680@gapps.yrdsb.ca>
2024-01-10 13:09:40 -0500

    Make the character moving
    move to the correct location.

---
+++ girl.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class girl here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class girl extends Actor
+{
+    /**
+     * Act - do whatever the girl wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        move();
+        girl.setLocation(50,200);
+        
+    }
+}
